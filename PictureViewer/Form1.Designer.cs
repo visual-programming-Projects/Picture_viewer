24a347320f1aefd011a112b8f88942ac27a31cd0
Tuba Asif <68794712+TAProgramer@users.noreply.github.com>
2021-08-12 17:04:18 +0500

    pic Viewer slide show

---
+++ PictureViewer/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace PictureViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PreviousBtn = new System.Windows.Forms.Button();
+            this.NextBtn = new System.Windows.Forms.Button();
+            this.CloseBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Play_btn = new System.Windows.Forms.Button();
+            this.Pause_btn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PreviousBtn
+            // 
+            this.PreviousBtn.Location = new System.Drawing.Point(40, 61);
+            this.PreviousBtn.Name = "PreviousBtn";
+            this.PreviousBtn.Size = new System.Drawing.Size(108, 34);
+            this.PreviousBtn.TabIndex = 0;
+            this.PreviousBtn.Text = "Previous";
+            this.PreviousBtn.UseVisualStyleBackColor = true;
+            this.PreviousBtn.Click += new System.EventHandler(this.PreviousBtn_Click);
+            // 
+            // NextBtn
+            // 
+            this.NextBtn.Location = new System.Drawing.Point(217, 61);
+            this.NextBtn.Name = "NextBtn";
+            this.NextBtn.Size = new System.Drawing.Size(108, 34);
+            this.NextBtn.TabIndex = 1;
+            this.NextBtn.Text = "Next";
+            this.NextBtn.UseVisualStyleBackColor = true;
+            this.NextBtn.Click += new System.EventHandler(this.NextBtn_Click);
+            // 
+            // CloseBtn
+            // 
+            this.CloseBtn.Location = new System.Drawing.Point(400, 61);
+            this.CloseBtn.Name = "CloseBtn";
+            this.CloseBtn.Size = new System.Drawing.Size(108, 34);
+            this.CloseBtn.TabIndex = 2;
+            this.CloseBtn.Text = "Close";
+            this.CloseBtn.UseVisualStyleBackColor = true;
+            this.CloseBtn.Click += new System.EventHandler(this.CloseBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(40, 135);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(468, 158);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Play_btn
+            // 
+            this.Play_btn.Location = new System.Drawing.Point(99, 314);
+            this.Play_btn.Name = "Play_btn";
+            this.Play_btn.Size = new System.Drawing.Size(135, 34);
+            this.Play_btn.TabIndex = 4;
+            this.Play_btn.Text = "Play";
+            this.Play_btn.UseVisualStyleBackColor = true;
+            this.Play_btn.Click += new System.EventHandler(this.Play_btn_Click);
+            // 
+            // Pause_btn
+            // 
+            this.Pause_btn.Location = new System.Drawing.Point(269, 314);
+            this.Pause_btn.Name = "Pause_btn";
+            this.Pause_btn.Size = new System.Drawing.Size(139, 34);
+            this.Pause_btn.TabIndex = 5;
+            this.Pause_btn.Text = "Pause";
+            this.Pause_btn.UseVisualStyleBackColor = true;
+            this.Pause_btn.Click += new System.EventHandler(this.Pause_btn_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 372);
+            this.Controls.Add(this.Pause_btn);
+            this.Controls.Add(this.Play_btn);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.CloseBtn);
+            this.Controls.Add(this.NextBtn);
+            this.Controls.Add(this.PreviousBtn);
+            this.Name = "Form1";
+            this.Text = "Picture Viewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PreviousBtn;
+        private System.Windows.Forms.Button NextBtn;
+        private System.Windows.Forms.Button CloseBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Play_btn;
+        private System.Windows.Forms.Button Pause_btn;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PictureViewer/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PictureViewer
+{
+    public partial class Form1 : Form
+    {   static int[] array1= new int[] { 1,2,3,4};
+        static int counter=0;
+     
+        static Boolean checkFlow = false;
+        
+        List<string> imagePaths = new List<string>();
+        List<string> extractedPath = new List<string>();
+
+        public Form1()
+        {   
+            InitializeComponent();
+        }
+
+        public void ImagesData() {
+            string givenPat = @".*?(\/[\/\w\.]+)[\s\?]?.*";
+            Regex pattern = new Regex(givenPat);
+
+            try {
+                string[] storelines = File.ReadAllLines(@"F:\5th semester\visual programming\Lab_Tasks\PictureViewer\PictureViewer\ImagesDataFolder\ImagesPath.xml");
+                foreach (string line in storelines)
+                {
+                    Match match = pattern.Match(line);
+                    if (match.Success)
+                    {
+                        imagePaths.Add(match.Value);
+                    }
+                    else {
+                      
+                    }
+
+                }
+
+            }
+            catch (Exception ex) { }
+
+           
+        }
+
+        public void methodToExtractPath() {
+            for (int i = 0; i < imagePaths.Count-1; i++) {
+                string removeStartTag = imagePaths.ElementAt(i).Remove(0,9);
+                
+                string bothTagsremoval = removeStartTag.Remove(removeStartTag.Length-9,9);
+                
+                extractedPath.Add(bothTagsremoval);
+               
+               
+            }
+            }
+        
+        
+
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void PreviousBtn_Click(object sender, EventArgs e)
+        {
+            counter--;
+            if (counter < 0)
+            {
+                MessageBox.Show("Image Not Available");
+
+            }
+            else {
+                this.pictureBox1.Image = Image.FromFile(extractedPath[counter]);
+
+
+            }
+
+            checkFlow = true;
+
+        }
+
+        private void NextBtn_Click(object sender, EventArgs e)
+        {
+            counter++;
+            if (counter >=4)
+            {
+                MessageBox.Show("Image Not Available");
+
+            }
+            else
+            {
+                this.pictureBox1.Image = Image.FromFile(extractedPath[counter]);
+
+            }
+            checkFlow = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ImagesData();
+            
+            methodToExtractPath();
+
+
+            this.pictureBox1.Image = Image.FromFile(extractedPath[0]);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+        }
+
+        private void Play_btn_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void Pause_btn_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (checkFlow)
+            {
+                
+                if (counter < 0)
+                {
+                    counter = 4;
+
+                }
+                else
+                {
+                    this.pictureBox1.Image = Image.FromFile(extractedPath[counter]);
+
+                }
+                counter--;
+            }
+            else {
+                
+                if (counter >= 4)
+                {
+                    counter = -1;
+
+                }
+                else
+                {
+                    this.pictureBox1.Image = Image.FromFile(extractedPath[counter]);
+
+                }
+                counter++;
+            }
+        }
+    }
+}
